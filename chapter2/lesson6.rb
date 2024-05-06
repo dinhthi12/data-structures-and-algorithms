9a2760d9311291d10674c2c52eea7fd46ee7ef40
dinhthi12 <thi884641@gmail.com>
2024-05-06 22:53:03 +0700

    Update solution

---
+++ chapter2/lesson6.rb
@@ -0,0 +1,13 @@
+# Tìm số nɡuyên dươnɡ n nhỏ nhất sɑo cho 1 + 2 + … + n > 10000
+
+def solution
+  sum = 0
+  n = 1
+
+  while sum < 10000
+    sum += n
+    n += 1
+  end
+
+  return n -1
+end
---
+++ test/chapter2/lesson6.rb
@@ -0,0 +1,7 @@
+require_relative '../../chapter2/lesson6'
+
+RSpec.describe 'solution' do
+  it 'returns the smallest integer n such that the sum of integers from 1 to n is greater than 10000' do
+    expect(solution).to eq(141)
+  end
+end
