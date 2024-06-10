d1bf972bf961df058e0557e47d573fba197aef9a
dinhthi12 <thi884641@gmail.com>
2024-06-10 15:16:03 +0700

    [Update] commit

---
+++ leetcode/twosum.rb
@@ -0,0 +1,15 @@
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer[]}
+def two_sum(nums, target)
+  hash = {}
+
+  nums.each_with_index do |num, index|
+    complement = target - num
+    if hash.key?(complement)
+      return [hash[complement], index]
+    end
+    hash[num] = index
+  end
+  []
+end
