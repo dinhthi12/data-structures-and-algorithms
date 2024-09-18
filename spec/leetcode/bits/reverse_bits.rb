6fbdf1933d0c08f24ec932c45a473d1e5c67fbf4
dinhthi12 <thi884641@gmail.com>
2024-09-18 15:38:51 +0700

    [Update] solution for exercises reverse_bits

---
+++ spec/leetcode/bits/reverse_bits.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require_relative '../../../src/leetcode/bits/reverse_bits'
+
+RSpec.describe 'reverse_bits' do
+  describe '#reverse_bits' do
+    it 'reverses the bits of 0b00000010100101000001111010011100' do
+      # Input: 0b00000010100101000001111010011100 (binary) = 43261596 (decimal)
+      # Expected output: 964176192 (binary: 0b00111001011110000010100101000000)
+      expect(reverse_bits(0b00000010100101000001111010011100)).to eq(964176192)
+    end
+
+    it 'reverses the bits of 0b11111111111111111111111111111101' do
+      # Input: 0b11111111111111111111111111111101 (binary) = 4294967293 (decimal)
+      # Expected output: 3221225471 (binary: 0b10111111111111111111111111111111)
+      expect(reverse_bits(0b11111111111111111111111111111101)).to eq(3221225471)
+    end
+
+    it 'reverses the bits of 0b00000000000000000000000000000000' do
+      # Input: 0b00000000000000000000000000000000 (binary) = 0 (decimal)
+      # Expected output: 0 (binary: 0b00000000000000000000000000000000)
+      expect(reverse_bits(0b00000000000000000000000000000000)).to eq(0)
+    end
+
+    it 'reverses the bits of 0b10000000000000000000000000000000' do
+      # Input: 0b10000000000000000000000000000000 (binary) = 2147483648 (decimal)
+      # Expected output: 1 (binary: 0b00000000000000000000000000000001)
+      expect(reverse_bits(0b10000000000000000000000000000000)).to eq(1)
+    end
+  end
+end
