eb479493b56456aeae334d773b1841092ab126bb
dinhthi12 <thi884641@gmail.com>
2024-10-14 14:07:33 +0700

    Make solution - exercises

---
+++ src/exercises/chapter1/Lesson1.ts
@@ -0,0 +1,13 @@
+// Lesson 1: Calculate S(n) = 1 + 2 + 3 + … + n
+
+export class Lesson1 {
+
+  solution(n: number) {
+    let result = 0;
+
+    for (let i = 0; i <= n; i++) {
+      result += i;
+    }
+    return result;
+  }
+}
