9503b481b4d22209c6734654db0f7e423f29820c
dinhthi12 <thi884641@gmail.com>
2025-05-14 14:15:53 +0700

    Update

---
+++ FullScource/VariablesTypesandConstants/index.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+const jwtToken string = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+
+func main() {
+	var username string = "Bro"
+	fmt.Println(username)
+	fmt.Printf("Variable is of type: %T \n", username)
+
+	var isLoggedIn bool = false
+	fmt.Println(isLoggedIn)
+	fmt.Printf("Variable is of type: %T \n", isLoggedIn)
+
+	var smallVal uint8 = 255
+	fmt.Println(smallVal)
+	fmt.Printf("Variable is of type: %T \n", smallVal)
+
+	numberOfUser := 300.00
+	fmt.Printf("Variable is of type: %T \n", numberOfUser)
+
+	fmt.Printf("Variable is of type: %T \n", jwtToken)
+}
