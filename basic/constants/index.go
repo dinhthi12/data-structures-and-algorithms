3c0b5facf6b3c68c4e6a7fe8d3b32e201afac4df
dinhthi12 <thi884641@gmail.com>
2025-05-16 09:50:05 +0700

    Update

---
+++ basic/constants/index.go
@@ -0,0 +1,33 @@
+package main
+
+func main() {
+	/**
+	Cannot be redeclared/reassigned
+	Can only hod scalar values
+	Can hold large high precision numbers
+	Can be created from expressions of other constants
+	Can be untyped (kind)
+	Constants exits only during compilation time
+	*/
+
+	//one line untyped
+	const identifierInt = 10
+
+	//one line typed
+	const identifier string = "Hello World!"
+
+	//multi line (group) mixed
+	const (
+		//numbers
+		number1 int     = 10
+		number2 float64 = 22.2
+
+		//strings
+		string1 = "Hello"
+		string2 = "World"
+
+		char1 byte = 'A'
+
+		complexNum complex128 = 1 + 0i
+	)
+}
